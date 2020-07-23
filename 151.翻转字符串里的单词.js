de4880e190f78609baa33f6fd4fe2bdd796cc824
“southchen <divetothesea@gmail.com>
2020-07-23 15:41:55 +0800

    'array'

--- 151.翻转字符串里的单词.js
+++ 151.翻转字符串里的单词.js
@@ -9,28 +9,28 @@
  * @param {string} s
  * @return {string}
  */
-// var reverseWords = function (s) {
-//   s = s.split(' ');
-//   let temp = [];
-//   for (let i = s.length - 1; i >= 0; i--) {
-//     if (s[i] != '') temp.push(s[i]);
-//   }
-//   temp = temp.join(' ');
-//   return temp;
-// };
 var reverseWords = function (s) {
   s = s.split(' ');
-  let stack = [];
-  for (let word of s) {
-    stack.push(word);
+  let temp = [];
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] != '') temp.push(s[i]);
   }
-  s.lenght = 0;
-  while (stack.length > 0) {
-    let cur = stack.pop();
-    if (cur != '') s.push(cur);
-  }
-  s = s.join(' ');
-  return s;
+  temp = temp.join(' ');
+  return temp;
 };
+// var reverseWords = function (s) {
+//   s = s.split(' ');
+//   let stack = [];
+//   for (let word of s) {
+//     stack.push(word);
+//   }
+//   s.lenght = 0;
+//   while (stack.length > 0) {
+//     let cur = stack.pop();
+//     if (cur != '') s.push(cur);
+//   }
+//   s = s.join(' ');
+//   return s;
+// };
 
 // @lc code=end
